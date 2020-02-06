d15f607a673c742f440439076151e0f31c5ba43c
Scott Solomon <1718@holbertonschool.com>
2020-02-06 02:00:56 +0000

    Fix 4 - add comment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
+/* This is a puts file to show how it works */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-char monkey[] = "\"Programming is like building a multilingual puzzle";
+	char monkey[] = "\"Programming is like building a multilingual puzzle";
 
-puts(monkey);
+	puts(monkey);
 
-return (0);
+	return (0);
 
 }
