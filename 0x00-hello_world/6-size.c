fda2434c6a5782fe39c361127bb0f9d7dd82af70
Scott Solomon <1718@holbertonschool.com>
2020-02-06 17:31:09 +0000

    Fix 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
+ * Print size of verious variables
+ * 
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -11,5 +14,6 @@ int main(void)
 	printf("Size of a long int is: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+
 	return (0);
 }
