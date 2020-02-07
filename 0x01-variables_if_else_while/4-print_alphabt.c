6ca3e32ac157ee79ca9db96c9b7d4adb74c51cdb
Scott Solomon <1718@holbertonschool.com>
2020-02-07 03:14:25 +0000

    Add 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Prints alphabet in lower case
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch = 'q')
+		{
+			return (0);
+		}
+		else if (ch = 'e')
+		{
+			return (0);
+		}
+		else
+		{
+			putchar(ch);
+		}
+	}
+	for
+	putchar('\n');
+	return (0);
+}
