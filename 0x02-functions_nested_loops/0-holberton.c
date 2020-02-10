c86c619de6be55768e674c664f63a39170623c7a
Scott Solomon <1718@holbertonschool.com>
2020-02-10 18:51:38 +0000

    Add 0x02, README.md, 0, header file

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c = "Holberton";
+
+	for (c = 0; c < 1; c++)
+	{
+		_putchar(c);
+	}
+	_putchar("\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+Learning about functions and nested loops
+2/10/2020
