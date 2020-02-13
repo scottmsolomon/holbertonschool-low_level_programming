98875dab256839d99e1a078eb07435f19d633597
Scott Solomon <1718@holbertonschool.com>
2020-02-13 19:55:43 +0000

    Fix 11 - Added code

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints numbers to 98
+ * @n: inputted number
+ *
+ * Prints all numbers to 98 starting with inputted number
+ * Return: 0 (success)
+ */
+void print_to_98(int n)
+{
+	int m;
+
+	for (m = n; m < 98; m++)
+	{
+		printf("%d, ", m);
+	/*	printf("98 \n"); */
+	}
+	for (m = n; m > 98; m--)
+	{
+		printf("%d, ", m);
+	/*	printf("98 \n"); */
+	}
+	printf("98 \n");
+}
