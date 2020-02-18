73e3a92c5ea1508f00e32677b034f00bdfec2df5
Scott Solomon <1718@holbertonschool.com>
2020-02-18 03:30:58 +0000

    Add 2 - incorrect

---
+++ 0x05-pointers_array_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * swap_int - Swaps the integer values
+ * @a: first integer
+ * @b: second integer
+ *
+ * Swaps one integer with another
+ * Return 0 Always (Success)
+ */
+
+int _strlen(char *s)
+{
+	int str[];
+	int n;
+
+	while (str[n] != '\0')
+	{
+		n++;
+	}
+
+	return (n);
+}
